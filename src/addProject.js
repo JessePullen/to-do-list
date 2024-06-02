4ae3e268981de068191186a6b9e298072fed9803
Jesse <jessepullen@icloud.com>
2024-06-02 18:38:15 +1000

    Add module addProject and consolidate repeated CSS

---
+++ src/addProject.js
@@ -0,0 +1,40 @@
+let formIsOpen = false;
+
+function addProject() {
+    if (formIsOpen === false) {
+        projectForm();
+        formIsOpen = true;
+    }
+}
+
+function projectForm() {
+    const sidebar = document.querySelector('.projects');
+    
+    const form = document.createElement('div');
+    form.classList.add('project-form');
+    sidebar.appendChild(form);
+
+    const projectInput = document.createElement('input');
+    projectInput.classList.add('project-input');
+
+    const addProject = document.createElement('button');
+    addProject.textContent = 'Add Project';
+    addProject.classList.add('add-project-button');
+
+    const cancel = document.createElement('button');
+    cancel.textContent = 'Cancel';
+    cancel.classList.add('cancel-button');
+
+    form.appendChild(projectInput);
+    form.appendChild(addProject);
+    form.appendChild(cancel);
+
+    cancel.addEventListener('click', () => {
+        projectInput.remove();
+        addProject.remove();
+        cancel.remove();
+        formIsOpen = false
+    });
+}
+
+export { addProject };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import './style.css';
 import { openForm } from './taskForm';
+import { addProject } from './addProject';
 
 const newTaskButton = document.querySelector('.new-task');
+const newProjectButton = document.querySelector('.new-project');
 
 newTaskButton.addEventListener('click', () => {
     openForm();
+});
+
+newProjectButton.addEventListener('click', () => {
+    addProject();
 });
